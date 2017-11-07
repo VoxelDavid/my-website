88060708686eead4d718a2358e8b9cf510f97b50
David Minnerly <voxeldavid@gmail.com>
2017-11-07 18:53:41 -0500

    Revise intro

--- src/components/content/Intro.jsx
+++ src/components/content/Intro.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-import links from 'css/components/_links.scss';
-import { Section, Content, Title } from '../layout';
+import { Section, Content } from '../layout';
+import ActionButton from '../ActionButton';
+import MyPicture from '../MyPicture';
 
 export default class Intro extends Component {
   render() {
     return (
       <Section>
-        <Content>
-          <Title>About</Title>
+        <Content reduced>
+          <h2>I’m David Minnerly, a freelance 3D modeler and programmer. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</h2>
 
-          <p>Hey, nice to meet you, I'm David Minnerly. I'm a programmer, 3D modeler, and I also make websites on occasion.</p>
-
-          <p>I'm fluent in Lua, and I can hold my ground in JavaScript and Python. I frequently post about what I'm up to on <a href="https://twitter.com/vocksel_" className={links.twitter}>Twitter <i className="fa fa-twitter" /></a> and many of my projects are open-sourced on <a href="https://github.com/vocksel" className={links.github}>GitHub <i className="fa fa-github" /></a>.</p>
-
-          <p>Feel free to <Link to="/contact" className={links.contact}>contact me <i className="fa fa-paper-plane-o" /></Link> with anything on your mind, I'd love to chat and get to know you.</p>
+          <Link to="/work"><ActionButton>View my work</ActionButton></Link>
         </Content>
+
+        <Content aside><MyPicture /></Content>
       </Section>
     );
   }
