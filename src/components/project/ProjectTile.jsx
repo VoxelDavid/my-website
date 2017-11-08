9430204eb0f2b0daa355831b3b809a6ec73e2b4f
David Minnerly <voxeldavid@gmail.com>
2017-11-08 09:01:13 -0500

    Update how selected projects are displayed

--- src/components/project/ProjectTile.jsx
+++ src/components/project/ProjectTile.jsx
@@ -3,15 +3,36 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
 import styles from './ProjectTile.scss';
+import ProjectPreview from './ProjectPreview';
+import ActionButton from '../ActionButton';
 import { Content } from '../layout';
 
 export default class ProjectTile extends Component {
+  // For right now we're just grabbing the first paragraph from the project's
+  // description. Later we'll have a dedicated bio to display on the homepage
+  // for selected work.
+  getShortBio() {
+    return this.props.shortBio.props.children[0];
+  }
+
   render() {
     return (
-      <Content className={styles.base}>
-        <Link to={`/projects/${this.props.slug}`}>
-          <div className={styles.image} style={{ backgroundImage: `url(${this.props.image})` }} />
-        </Link>
+      <Content>
+        <div className={styles.base}>
+          <div className={styles.bio}>
+            <h2>{this.props.title}</h2>
+
+            {this.getShortBio()}
+
+            <Link to={`/projects/${this.props.slug}`}>
+              <ActionButton>View Project</ActionButton>
+            </Link>
+          </div>
+
+          <Link to={`/projects/${this.props.slug}`} className={styles.preview}>
+            <ProjectPreview image={this.props.image} />
+          </Link>
+        </div>
       </Content>
     );
   }
