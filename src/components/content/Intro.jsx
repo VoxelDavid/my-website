33577f663fe827c31eeb91adadbeef6fdb2645fd
David Minnerly <voxeldavid@gmail.com>
2017-11-08 08:47:45 -0500

    Remove ActionButton from intro

--- src/components/content/Intro.jsx
+++ src/components/content/Intro.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import { Section, Content } from '../layout';
-import ActionButton from '../ActionButton';
 import MyPicture from '../MyPicture';
 
 export default class Intro extends Component {
@@ -11,8 +10,6 @@ export default class Intro extends Component {
       <Section>
         <Content reduced>
           <h2>I’m David Minnerly, a freelance 3D modeler and programmer. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</h2>
-
-          <Link to="/work"><ActionButton>View my work</ActionButton></Link>
         </Content>
 
         <Content aside><MyPicture /></Content>
