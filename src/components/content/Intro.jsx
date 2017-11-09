1c900911ff0b84a1fcd976c0faf365b3e46a5f21
David Minnerly <voxeldavid@gmail.com>
2017-11-08 21:26:24 -0500

    Revamp intro

--- src/components/content/Intro.jsx
+++ src/components/content/Intro.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-import MyPicture from '../MyPicture';
 import Container from '../layout/Container';
+import styles from './Intro.scss';
 
 export default class Intro extends Component {
   render() {
     return (
-      <Container>
-        <p className={styles.bio}>I’m David Minnerly, a freelance 3D modeler and programmer. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</p>
-        <MyPicture />
-      </Container>
+      <div className={styles.base}>
+        <div className={styles.bg} />
+
+        <Container>
+          <p className={styles.bio}>I’m David Minnerly, a freelance 3D modeler and programmer. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</p>
+        </Container>
+      </div>
     );
   }
 }
