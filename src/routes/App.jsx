b61be6b65cad63e2e4d0e5243f477bdf7e6bcc82
David Minnerly <voxeldavid@gmail.com>
2017-11-15 09:29:46 -0500

    Readd the header

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 import Footer from 'components/Footer';
+import Header from 'components/Header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
+
         {this.props.children}
 
         <Footer />
