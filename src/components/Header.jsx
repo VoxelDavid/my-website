d55923ff9833c8ca1576bb5f01e34a75cfc71555
David Minnerly <voxeldavid@gmail.com>
2017-11-15 09:27:04 -0500

    Remove my name from the header
    Turns out we don't need any styles either. Gonna be applying padding to
    the body to push things down, but the header itself is good as is.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
-import nav from './navigation/NavLink.scss';
-import styles from './Header.scss';
 import Container from './layout/Container';
 import PrimaryNav from './navigation/PrimaryNav';
 
 export default class Header extends Component {
   render() {
     return (
-      <Container className={styles.base}>
-        <div className={styles.name}>
-          <Link to="/" className={nav.link}>David Minnerly</Link>
-        </div>
-
-        <div className={styles.nav}>
-          <PrimaryNav />
-        </div>
+      <Container>
+        <PrimaryNav />
       </Container>
     );
   }
