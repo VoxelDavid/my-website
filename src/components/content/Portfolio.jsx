5f7e838c259ec3f9760770f28d699ce9f9aa2c1e
David Minnerly <voxeldavid@gmail.com>
2017-11-15 09:23:27 -0500

    Use an array to display selected work

--- src/components/content/Portfolio.jsx
+++ src/components/content/Portfolio.jsx
@@ -3,15 +3,21 @@ import React, { Component } from 'react';
 import { Content, Title } from '../layout';
 import Container from '../layout/Container';
 import ProjectTile from '../project/ProjectTile';
-import projects from 'projects';
 
 function isOdd(n) {
   return n % 2;
 }
 
+const selectedWork = [
+  // These are displayed in order.
+  require('projects/apartment').default,
+  require('projects/echo-ridge').default,
+  require('projects/mirage-isle').default
+];
+
 export default class Portfolio extends Component {
   getProjectTiles() {
-    return projects.map((project, index) =>
+    return selectedWork.map((project, index) =>
       <ProjectTile key={index} title={project.name} slug={project.slug}
         shortBio={project.description} image={project.images[0]}
         inverted={isOdd(index)} />
