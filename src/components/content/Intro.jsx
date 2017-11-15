ce86e951fb787038980ad33ae9a8eff5247c1a2d
David Minnerly <voxeldavid@gmail.com>
2017-11-15 09:10:03 -0500

    Update intro

--- src/components/content/Intro.jsx
+++ src/components/content/Intro.jsx
@@ -8,10 +8,10 @@ export default class Intro extends Component {
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.bg} />
-
         <Container>
-          <p className={styles.bio}>I’m David Minnerly, a freelance 3D modeler and programmer. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</p>
+          <div className={styles.intro}>
+            <p className={styles.bio}>I'm David Minnerly, a freelance 3D modeler and programmer living in New York. Currently working on <Link to="/projects/wild-revolvers">Wild Revolvers</Link> and the unreleased <Link to="/projects/super-gun-fight">Super Gun Fight</Link>.</p>
+          </div>
         </Container>
       </div>
     );
